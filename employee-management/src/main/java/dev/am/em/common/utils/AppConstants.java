cf293891df93b6c8bc37275f0bd483fb550707a1
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-02-19 23:34:20 -0400

    Initial commit, adding db connection and config profiles

---
+++ employee-management/src/main/java/dev/am/em/common/utils/AppConstants.java
@@ -0,0 +1,7 @@
+package dev.am.em.common.utils;
+
+public interface AppConstants {
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_TEST = "test";
+    String SPRING_PROFILE_PRODUCTION = "prod";
+}
